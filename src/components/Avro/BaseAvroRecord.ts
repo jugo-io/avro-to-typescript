f8a198e5ad3e3555235840490382ba19d75e708b
Levon Mkrtchyan <levonmk@users.noreply.github.com>
2020-03-19 11:40:25 -0400

    Remove unused code from BaseAvroRecord and memoize dependency (#6)

--- src/components/Avro/BaseAvroRecord.ts
+++ src/components/Avro/BaseAvroRecord.ts
@@ -1,26 +1,9 @@
-import * as avro from "avsc";
-import { Schema, Type } from "avsc";
-import { Memoize } from "typescript-memoize";
 import { AvroRecord } from "./AvroRecord";
 
 export abstract class BaseAvroRecord implements AvroRecord {
 
     public static readonly subject: string = "";
     public static readonly schema = {};
-
-    @Memoize((schema: any) => {
-        return schema.namespace + schema.name;
-    })
-    public static getTypeForSchema(schema: any): Type {
-        return avro.Type.forSchema(schema);
-    }
-
-    @Memoize((schema: any) => {
-        return schema.namespace + schema.name;
-    })
-    public static createTypeResolver(baseType: Type, newType: Type): Type {
-        return baseType.createResolver(newType) as Type;
-    }
 
     public abstract schema(): any;
 
